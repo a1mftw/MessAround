8763c55c0731e992a840111726466035b5f4d554
Zin <bernardoreis2000@hotmail.com>
2020-09-18 21:03:10 +0100

    Movement, Collision, Pop Up for Transition, and Scene Load

---
+++ PixelStorm/Assets/Scripts/EnablePopUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePopUp : MonoBehaviour
+{
+    public GameObject popup;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Reached Entrance!");
+            popup.SetActive(true);
+        }
+    }
+}
